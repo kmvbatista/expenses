7d4b6aa6f1a87b80268c3d9cab0d7fcef6cc6b6d
Kennedy <kennedymessias.vb@gmail.com>
2020-01-28 13:47:46 -0300

    creating charts

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,27 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      return {'day': DateFormat.E(weekDay), 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactionValues(),
+      ),
+    );
+  }
+}
